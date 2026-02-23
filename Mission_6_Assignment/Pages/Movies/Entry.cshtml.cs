78a333643fcbf695b1d5083d950aaea74b5b9326
Dakota <Jmdowd2020@gmail.com>
2026-02-22 23:20:23 -0700

    pages and stuff

---
+++ Mission_6_Assignment/Pages/Movies/Entry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission_6_Assignment.Views.Movies
+{
+    public class EntryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
